d5164f929b0ac8db1589551bd9a2dcc749a03d83
zheng-wang-hexad <zheng.wang@hexad.de>
2022-11-09 09:54:38 +0100

    tmp

--- src/test/java/film/monorvo/email/PDFReaderTest.java
+++ src/test/java/film/monorvo/email/PDFReaderTest.java
@@ -1,13 +1,23 @@
 package film.monorvo.email;
 
-import junit.framework.TestCase;
+
 import org.junit.Test;
 
-public class PDFReaderTest extends TestCase {
-    private PDFReader reader = new PDFReader();
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+public class PDFReaderTest {
+    private EmailReader reader = new EmailReader();
 
     @Test
     public void readerPDFs() {
+        var time = Instant.now().getEpochSecond();
 
+        var localTime =  LocalDateTime.ofInstant(Instant.ofEpochMilli(time * 1000),
+                TimeZone.getDefault().toZoneId());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.println(localTime.format(formatter));
     }
 }
